ed078d2c78b1c5c00efc5501b101dfb5fb0269c3
Bruno Moraes <brunobva@debian-BULLSEYE-live-builder-AMD64>
2022-11-03 14:49:22 +0000

    Updated ReadMe

--- set-vars.sh
+++ set-vars.sh
@@ -1,4 +1,4 @@
 
-export awsAccessKey="AKIA3BDDMSB3ZDFHPLN3"
-export awsSecretKey="UlhQL0Y2idLwTeD9zbOiI3b0NeOLRheKh9GSw7yH"
+export awsAccessKey="<please add your AWS Access Key Here>"
+export awsSecretKey="<please add your AWS Access Key Here>"
 alias tf='docker run -ti --rm --name terraform -v /tmp:/bva terraform terraform'
